73a5522823e345417cf6a4e2c419147e40f68134
Puvel <puvelko@gmail.com>
2020-04-07 00:25:56 +0300

    promisification

---
+++ js/task-2.js
@@ -0,0 +1,47 @@
+//? Перепиши функцию toggleUserState() так,
+//? чтобы она не использовала callback-функцию callback,
+//? а принимала всего два параметра allUsers и userName и возвращала промис.
+'use strict';
+
+const users = [
+  { name: 'Mango', active: true },
+  { name: 'Poly', active: false },
+  { name: 'Ajax', active: true },
+  { name: 'Lux', active: false },
+];
+
+//! ========ИСХОДНАЯ ФУНКЦИЯ
+// const toggleUserState = (allUsers, userName, callback) => {
+//   const updatedUsers = allUsers.map(user =>
+//     user.name === userName ? { ...user, active: !user.active } : user,
+//   );
+
+//   callback(updatedUsers);
+// };
+
+//! ============НОВАЯ ФУНКЦИЯ
+const toggleUserState = (allUsers, userName) => {
+  const promise = new Promise((resolve, reject) => {
+    const updatedUsers = allUsers.map(user =>
+      user.name === userName ? { ...user, active: !user.active } : user,
+    );
+    resolve(updatedUsers);
+  });
+  return promise;
+};
+
+
+
+const logger = updatedUsers => console.table(updatedUsers);
+
+// /*
+//  * Сейчас работает так
+//  */
+// toggleUserState(users, 'Mango', logger);
+// toggleUserState(users, 'Lux', logger);
+
+/*
+ * Должно работать так
+ */
+toggleUserState(users, 'Mango').then(logger);
+toggleUserState(users, 'Lux').then(logger);
