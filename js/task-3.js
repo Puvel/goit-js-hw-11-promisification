1a90952df05df1a5b6332caefd6b0a5596aba48e
Puvel <puvelko@gmail.com>
2020-04-08 15:52:18 +0300

    fix

--- js/task-3.js
+++ js/task-3.js
@@ -7,7 +7,7 @@ const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-//! =============ИСХОДНАЯ ФУНКЦИЯ
+// // ! =============ИСХОДНАЯ ФУНКЦИЯ
 // const makeTransaction = (transaction, onSuccess, onError) => {
 //   const delay = randomIntegerFromInterval(200, 500);
 
@@ -30,7 +30,7 @@ const makeTransaction = transaction => {
       const canProcess = Math.random() > 0.3;
 
       if (canProcess) {
-        resolve(transaction.id, delay);
+        resolve({ id: transaction.id, time: delay });
       } else {
         reject(transaction.id);
       }
@@ -39,7 +39,7 @@ const makeTransaction = transaction => {
   return promise;
 };
 
-const logSuccess = (id, time) => {
+const logSuccess = ({ id, time }) => {
   console.log(`Transaction ${id} processed in ${time}ms`);
 };
 
@@ -58,18 +58,10 @@ const logError = id => {
 /*
  * Должно работать так
  */
-makeTransaction({ id: 70, amount: 150 })
-  .then(logSuccess)
-  .catch(logError);
+makeTransaction({ id: 70, amount: 150 }).then(logSuccess).catch(logError);
 
-makeTransaction({ id: 71, amount: 230 })
-  .then(logSuccess)
-  .catch(logError);
+makeTransaction({ id: 71, amount: 230 }).then(logSuccess).catch(logError);
 
-makeTransaction({ id: 72, amount: 75 })
-  .then(logSuccess)
-  .catch(logError);
+makeTransaction({ id: 72, amount: 75 }).then(logSuccess).catch(logError);
 
-makeTransaction({ id: 73, amount: 100 })
-  .then(logSuccess)
-  .catch(logError);
+makeTransaction({ id: 73, amount: 100 }).then(logSuccess).catch(logError);
